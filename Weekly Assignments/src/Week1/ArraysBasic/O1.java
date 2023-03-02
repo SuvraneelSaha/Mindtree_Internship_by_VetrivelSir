af1623189d7decc21ed237a2ae01e8c8d9692493
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-03-02 09:46:53 +0530

    Week 1 Array Basics Start

---
+++ Weekly Assignments/src/Week1/ArraysBasic/O1.java
@@ -0,0 +1,14 @@
+package Week1.ArraysBasic;
+
+import java.util.Scanner;
+
+public class O1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+
+    }
+
+}
