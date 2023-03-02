2004f8bafc33b8f4df46da6dfa8a4da1e29a5624
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-03-02 09:52:58 +0530

    Sum of Arrays Question Done

--- Weekly Assignments/src/Week1/ArraysBasic/O1.java
+++ Weekly Assignments/src/Week1/ArraysBasic/O1.java
@@ -1,12 +1,23 @@
 package Week1.ArraysBasic;
 
 import java.util.Scanner;
-
+// Sum of Arrays 
 public class O1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int sum = 0 ;
+        for(int i = 0 ; i<n;i++)
+        {
+
+            sum = sum + arr[i];
+        }
+        System.out.println("The sum of the elements in the array is "+sum);
+
 
 
     }
