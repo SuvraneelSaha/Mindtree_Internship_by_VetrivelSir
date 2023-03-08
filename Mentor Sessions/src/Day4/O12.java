36c24e647f93cbae2af4fb9cb827e6ec7130d307
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-03-08 19:41:42 +0530

    sme session sql done

--- Mentor Sessions/src/Day4/O12.java
+++ Mentor Sessions/src/Day4/O12.java
@@ -5,6 +5,23 @@ public class O12 {
 
     Designing of all the databases is done by
     ER Models
-    
+
+
+    what are the different types of
+    dbms keys
+    primary keys
+    candidate
+    foreign
+    composite
+
+    primary keys - are the distinct keys which uniquely identifies the rows
+    foreign keys - are the primary keys of other tables which are present in other table as a candidate key
+
+    candidate key - it is a normal key or column by which we can identify an entry or row
+
+    composite key - it is a combination of primary key and a candidate key so to evauluate or figure out a row
+
+
+
      */
 }
---
+++ Mentor Sessions/src/Day4/O13.java
@@ -0,0 +1,31 @@
+package Day4;
+
+public class O13 {
+    /*
+
+    select now();
+    select curdate();
+    select curtime();
+    this above methods are for checking time
+
+    // Subqueries
+    it is an combination of two queries
+    firstly the inner query will executed
+    then secondly the outer query will be executed
+
+    ex of a subquery -
+    select * from p1 where id in ( select idnew_table from new_table);
+    this is first execute the inner query and get it executed then it will
+    execute the outer query
+
+    another subquery example
+    insert into p1(id) select idnew_table from new_table where idnew_table=4;
+    the p1 table will have values from the post tables
+
+
+    
+
+
+
+     */
+}
