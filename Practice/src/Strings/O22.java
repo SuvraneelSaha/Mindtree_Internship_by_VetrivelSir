965eb53c22e30b52ccc9a11003087e372ded06b5
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-03-18 15:15:52 +0530

    done whole strings basics 🎯

--- Practice/src/Strings/O22.java
+++ Practice/src/Strings/O22.java
@@ -3,7 +3,7 @@ package Strings;
 public class O22 {
     public static void main(String[] args) {
         String str = "I love India";
-        System.out.println(str.charAt(0));
+        System.out.println(Character.toLowerCase(str.charAt(0)));
 
     }
 }
