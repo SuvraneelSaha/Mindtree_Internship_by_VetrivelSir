8050f1e5f5d67ff4add1ac6b8ac95cba37e40a73
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2023-03-18 14:41:22 +0530

    first letter of the word being in capital letter

--- Practice/src/Strings/O21.java
+++ Practice/src/Strings/O21.java
@@ -2,10 +2,19 @@ package Strings;
 
 public class O21 {
     public static void main(String[] args) {
-        String str = "I love India";
-        str = str+" ";
+        String str = "i love india";
+
         String word  ="";
-        for (int i = 0; i < str.length(); i++)
+        char c = str.charAt(0);
+        if(c >='a'&& c <='z')
+        {
+            word = word +(char)(c -32);
+        }
+        else
+        {
+            word = word + c;
+        }
+        for (int i = 1; i < str.length(); i++)
         {
                 char ch = str.charAt(i);
                 if(ch!=' ')
@@ -14,9 +23,12 @@ public class O21 {
                 }
                 else
                 {
-                    System.out.println();
+                    System.out.print(word + " ");
                     word="";
+                    word = word +Character.toUpperCase(str.charAt(i+1));
+                    i++; // if not then we will get the same i for 2nd time
                 }
         }
+        System.out.print(word);
     }
 }
